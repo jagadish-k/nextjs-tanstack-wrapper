8f36af84d9bb5e2e0edb66f014c71beae4de2ee7
Jagadish K <kasijagadish@gmail.com>
2024-03-24 11:52:58 +0530

    Add basePath to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
+const repositoryName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: repositoryName ? `/${repositoryName}` : '',
   output: 'export',
   reactStrictMode: true,
 };
